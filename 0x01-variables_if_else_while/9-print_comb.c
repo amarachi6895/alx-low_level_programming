4575730c96e99e31c96a9c8f5691d7834cf78d92
amarachi6895 <amarisrichard@gmail.com>
2024-08-03 09:45:25 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single-digit numbers from
+ * 0 to 9, separated by a comma and a space, followed by a new line. Uses
+ * putchar only four times and does not use any char variables.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 9; i++)
+{
+putchar('0' + i);          /* Print the current digit */
+putchar(',');              /* Print the comma */
+putchar(' ');              /* Print the space */
+}
+putchar('9');                  /* Print the last digit */
+putchar('\n');                 /* Print the newline */
+
+return (0);
+}
