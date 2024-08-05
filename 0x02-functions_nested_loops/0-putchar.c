ba4a1895348b989da9b83c4d83bbde9383c5806e
amarachi6895 <amarisrichard@gmail.com>
2024-08-04 20:49:53 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <unistd.h> /* Include the unistd.h header for the write function */
+
+/* Prototype declaration for _putchar */
+int _putchar(char c);
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *str = "_putchar";
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+
+return (0);
+}
