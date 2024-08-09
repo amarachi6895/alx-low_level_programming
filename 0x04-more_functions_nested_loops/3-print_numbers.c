050e5dfbd7de925c8f031bf2a049552309890f1e
amarachi6895 <amarisrichard@gmail.com>
2024-08-09 04:58:31 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_numbers - prints the numbers from 0 to 9
- * followed by a new line
+ * followed by a dollar sign and a new line
  */
 void print_numbers(void)
 {
-	char num;
+	char numbers[] = "0123456789$";
+	int i;
 
-	for (num = '0'; num <= '9'; num++)
+	for (i = 0; i < 11; i++)
 	{
-	_putchar(num);
+	_putchar(numbers[i]);
 	}
-	_putchar('$');
-	_putchar('\n');
+	_putchar('\n'); /* Use this to add a new line */
 }
