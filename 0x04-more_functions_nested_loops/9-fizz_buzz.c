0d9998c3837da749e78ece6ec08146c4efb9ff67
amarachi6895 <amarisrichard@gmail.com>
2024-08-10 23:45:20 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program.
+ *
+ * Description:
+ * This program prints the numbers from 1 to 100. For each number:
+ * - Print "Fizz" if the number is divisible by 3,
+ * - Print "Buzz" if the number is divisible by 5,
+ * - Print "FizzBuzz" if the number is divisible by both 3 and 5,
+ * - Print the number itself if none of the above conditions are met.
+ * Numbers or words are separated by a space. The output ends with a newline.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
